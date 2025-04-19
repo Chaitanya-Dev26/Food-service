ba45587e4d16b92e2b2cf9ec55ce6d0d4d30d0bd
chaitanya.s <chaitanyashirsat62@gmail.com>
2025-04-19 23:24:27 +0530

    feat: add About page and linked it to from Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -18,7 +19,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Index />} /> {/* Will be replaced when services page is created */}
           <Route path="/menu" element={<Index />} /> {/* Will be replaced when menu page is created */}
-          <Route path="/about" element={<Index />} /> {/* Will be replaced when about page is created */}
+          <Route path="/about" element={<About />} /> {/* Will be replaced when about page is created */}
           <Route path="/contact" element={<Index />} /> {/* Will be replaced when contact page is created */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
